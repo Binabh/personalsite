ccf2c27b2f96dfa53cff57717df29c822c2d62a7
Binabh <binabhdevkota@gmail.com>
2020-07-26 04:01:29 +0000

    deploy the bundle

---
+++ docs/precache-manifest.66e93c8aec285d7b285165c79aca8445.js
@@ -0,0 +1,66 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37a1fe380e83ac3d72c9e31aba3be234",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71e4c7657a47889683db",
+    "url": "/static/css/main.d702d3c1.chunk.css"
+  },
+  {
+    "revision": "0ecbb98a556ed9e2ac63",
+    "url": "/static/js/2.c956f2ce.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.c956f2ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71e4c7657a47889683db",
+    "url": "/static/js/main.f29d20cc.chunk.js"
+  },
+  {
+    "revision": "420d11f43645d04fc865",
+    "url": "/static/js/runtime-main.9b126251.js"
+  },
+  {
+    "revision": "d7a0592543237891feba32df536958ed",
+    "url": "/static/media/Tec_maps_of_nepal.d7a05925.png"
+  },
+  {
+    "revision": "fb9759c1049cd1a327825bf5b0cc9e98",
+    "url": "/static/media/adobe-xd-1.fb9759c1.svg"
+  },
+  {
+    "revision": "745526f7403bfa064821fbdff02f42d1",
+    "url": "/static/media/coding_illustration.745526f7.svg"
+  },
+  {
+    "revision": "c68a1876675fc9089863dc1245dc6d8b",
+    "url": "/static/media/flutter.c68a1876.svg"
+  },
+  {
+    "revision": "1c880b53942860b984dab7182686aae7",
+    "url": "/static/media/inkscape-logo.1c880b53.svg"
+  },
+  {
+    "revision": "2475a5ad63a8615ea2ce910f3a37b009",
+    "url": "/static/media/leafletjs-icon.2475a5ad.svg"
+  },
+  {
+    "revision": "ab29e015524219b5de4460f4cb0a05be",
+    "url": "/static/media/openlayers.ab29e015.svg"
+  },
+  {
+    "revision": "a9b7e6811b06b0183a58d4da1f153207",
+    "url": "/static/media/python-5.a9b7e681.svg"
+  },
+  {
+    "revision": "e23ba8b1f5794ba4a17b0bd7f40744f1",
+    "url": "/static/media/qgis-icon128.e23ba8b1.svg"
+  },
+  {
+    "revision": "899e1db66fc2a41579d295579a41974e",
+    "url": "/static/media/react.899e1db6.svg"
+  }
+]);
